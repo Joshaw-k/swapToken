1f26100582928c34bd927baf54de87b0167bfd53
Joshaw-k <awinrin4bless@yahoo.com>
2023-09-10 21:00:24 +0100

    first commit

--- scripts/interact.ts
+++ scripts/interact.ts
@@ -3,17 +3,17 @@ import { ethers } from "hardhat";
 async function main() {
   const swapContract = await ethers.getContractAt(
     "ISwap",
-    "0xde79380fbd39e08150adaa5c6c9de3146f53029e"
+    "0xAc045B906ea570938BF937291515Be31B948c9c3"
   );
 
   const pevcoin = await ethers.getContractAt(
     "IMyERC20",
-    "0x840748f7fd3ea956e5f4c88001da5cc1abcbc038"
+    "0x893B25C508F0486Cd2C99b45a1EEA397784deF57"
   );
 
   const rollcoin = await ethers.getContractAt(
     "IMyERC20",
-    "0x1befe2d8417e22da2e0432560ef9b2ab68ab75ad"
+    "0x9A425368a516c6ab1a51a8659906D8eC2Def048c"
   );
 
   const pevAmount = ethers.parseUnits("1000", 8);
@@ -21,24 +21,24 @@ async function main() {
   const rollAmount = ethers.parseUnits("1000", 8);
 
   await pevcoin.approve(
-    "0xde79380fbd39e08150adaa5c6c9de3146f53029e",
+    "0xAc045B906ea570938BF937291515Be31B948c9c3",
     pevAmount
   );
 
   await rollcoin.approve(
-    "0xde79380fbd39e08150adaa5c6c9de3146f53029e",
+    "0xAc045B906ea570938BF937291515Be31B948c9c3",
     rollAmount
   );
   // console.log(swapContract);
   console.log(
     await pevcoin.allowance(
-      "0xf39fd6e51aad88f6f4ce6ab8827279cfffb92266",
-      "0xde79380fbd39e08150adaa5c6c9de3146f53029e"
+      "0xb5119738bb5fe8be39ab592539eaa66f03a77174",
+      "0xAc045B906ea570938BF937291515Be31B948c9c3"
     )
   );
   console.log(
     await rollcoin.allowance(
-      "0xf39fd6e51aad88f6f4ce6ab8827279cfffb92266",
+      "0xb5119738bb5fe8be39ab592539eaa66f03a77174",
       "0xde79380fbd39e08150adaa5c6c9de3146f53029e"
     )
   );
@@ -46,6 +46,7 @@ async function main() {
     ethers.parseUnits("100", 8),
     ethers.parseUnits("100", 8)
   );
+  await swapContract.swapPevtokenForRollTokens();
 }
 
 // We recommend this pattern to be able to use async/await everywhere
