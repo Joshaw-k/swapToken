8816c898874d5f969c2012b1f7f6cef77fce4470
Joshaw-k <awinrin4bless@yahoo.com>
2023-09-10 20:41:15 +0100

    first commit

--- README.md
+++ README.md
@@ -1,13 +1,5 @@
 # Sample Hardhat Project
 
-This project demonstrates a basic Hardhat use case. It comes with a sample contract, a test for that contract, and a script that deploys that contract.
+Contract Address: 0xAc045B906ea570938BF937291515Be31B948c9c3
 
-Try running some of the following tasks:
-
-```shell
-npx hardhat help
-npx hardhat test
-REPORT_GAS=true npx hardhat test
-npx hardhat node
-npx hardhat run scripts/deploy.ts
-```
+Thank you for this test 🙅 Dev Wura.
--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,16 +1,15 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
+import "dotenv/config";
 
 const config: HardhatUserConfig = {
   solidity: "0.8.19",
   networks: {
-    // sepolia: {
-    //   url: process.env.SEPOLIARPC,
-    //   //@ts-ignore
-    //   accounts: [
-    //     process.env.PRIVATE_KEY_1,
-    //   ],
-    // },
+    sepolia: {
+      url: process.env.SEPOLIARPC,
+      //@ts-ignore
+      accounts: [process.env.PRIVATE_KEY_1],
+    },
     hardhat: {
       forking: {
         url: "https://sepolia.infura.io/v3/cf05af5bacf84b28aa67c6dea5d1d5c2",
